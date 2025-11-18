55dafe0c8c3ecf91355958cb75d5bf0cb7a599b7
Matheus Mota <matheuzwork@gmail.com>
2025-11-17 21:48:49 -0300

    feat: melhoria de desempenho

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,34 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+// ================================
+// 🔹 Carrega APENAS os pesos essenciais no início
+//    (somente o que aparece acima da dobra)
+// ================================
 import "@fontsource/be-vietnam-pro/400.css";
-import "@fontsource/be-vietnam-pro/600.css";
-import "@fontsource/be-vietnam-pro/700.css";
-import "@fontsource/oswald/200.css";
 import "@fontsource/oswald/400.css";
-import "@fontsource/oswald/700.css";
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+
+if (typeof requestIdleCallback !== "undefined") {
+  requestIdleCallback(() => {
+    import("@fontsource/be-vietnam-pro/600.css");
+    import("@fontsource/be-vietnam-pro/700.css");
+    import("@fontsource/oswald/200.css");
+    import("@fontsource/oswald/700.css");
+  });
+} else {
+  setTimeout(() => {
+    import("@fontsource/be-vietnam-pro/600.css");
+    import("@fontsource/be-vietnam-pro/700.css");
+    import("@fontsource/oswald/200.css");
+    import("@fontsource/oswald/700.css");
+  }, 0);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
