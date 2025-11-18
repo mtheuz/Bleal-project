50dbcfc63519f1296e5e471ec7d000decfac8ffb
Matheus Mota <matheuzwork@gmail.com>
2025-11-17 22:32:03 -0300

    fix: teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import critical from "rollup-plugin-critical";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    critical({
+      criticalUrl: '/',          // URL da página que será analisada
+      criticalBase: './dist/',   // pasta de saída do build
+      criticalPages: [
+        { uri: '', template: 'index.html' }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
