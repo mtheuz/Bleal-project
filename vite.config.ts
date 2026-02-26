ee1574b5ea007c0303e1b2125906d1ecae7641f7
Matheus Mota <matheuzwork@gmail.com>
2026-02-26 12:26:01 -0300

    fix: critical

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import critical from "rollup-plugin-critical";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    critical({
-      criticalUrl: '/',          // URL da página que será analisada
-      criticalBase: './dist/',   // pasta de saída do build
-      criticalPages: [
-        { uri: '', template: 'index.html' }
-      ]
-    })
   ],
   resolve: {
     alias: {
